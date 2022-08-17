d4abb0351ceec4c100e5ff7ad15a86797e892e4c
Atevaz <101355785+Atevaz@users.noreply.github.com>
2022-08-17 16:39:33 +0200

    colors

---
+++ lib/presentation/view/customer_driver_header_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CustomerDriverHeaderView extends StatelessWidget {
+  const CustomerDriverHeaderView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+
+      ],
+    );
+  }
+}
