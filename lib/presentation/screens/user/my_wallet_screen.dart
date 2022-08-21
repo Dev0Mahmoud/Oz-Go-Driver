2619846bd2e57d3a6aa98f4c71f5995609f478a3
MohanadRaghebDev <mohanad.ragheb.95@gmail.com>
2022-08-21 17:13:09 +0200

    trip_request_screen

--- lib/presentation/screens/user/my_wallet_screen.dart
+++ lib/presentation/screens/user/my_wallet_screen.dart
@@ -4,114 +4,145 @@ import 'package:oz_go_driver/constants/assets_manager.dart';
 import 'package:oz_go_driver/presentation/styles/colors.dart';
 import 'package:oz_go_driver/presentation/widget/circle_avatar.dart';
 import 'package:oz_go_driver/presentation/widget/default_button.dart';
+import 'package:oz_go_driver/presentation/widget/default_indicator.dart';
 import 'package:oz_go_driver/presentation/widget/notification_componant.dart';
 
 import '../../widget/custom_app_bar.dart';
 
 class MyWalletScreen extends StatelessWidget {
-  const MyWalletScreen({Key? key}) : super(key: key);
-
+  MyWalletScreen({Key? key}) : super(key: key);
+  PageController controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 290.h,
-              padding: EdgeInsets.symmetric(
-                horizontal: 10.w,
-              ),
-              decoration: BoxDecoration(
-                gradient: AppGradient.primaryGradient,
-              ),
-              child: Column(
-                children: [
-                  Spacer(),
-                  Row(
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.arrow_back,
-                            color: AppColor.white,
-                          )),
-                      SizedBox(
-                        width: 20.w,
-                      ),
-                      Text(
-                        'My Wallet',
-                        style: TextStyle(
-                            color: AppColor.white,
-                            fontSize: 25.sp,
-                            fontFamily: 'Mont',
-                            fontWeight: FontWeight.w700),
-                      )
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10.h),
-                    child: DefaultCircleAvatar(
-                        imageName: AssetsManager.person, radius: 40.r),
-                  ),
-                  Text(
-                    'Hossam Saeed',
-                    style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 25.sp,
-                        fontFamily: 'Mont',
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Container(
-                width: 375.w,
-                height: 175.h,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              /// app bar
+              Container(
+                height: 250.h,
+                padding: EdgeInsets.symmetric(
+                  horizontal: 10.w,
+                ),
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(22),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(.5),
-                      offset: Offset(0.0, 1.0), //(x,y)
-                      blurRadius: 3.0,
-                    ),
-                  ],
+                  gradient: AppGradient.primaryGradient,
                 ),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Text(
-                      'Your Wallet balance',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontFamily: 'Mont',
-                          fontSize: 20.sp),
+                    Spacer(),
+                    Row(
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.arrow_back,
+                              color: AppColor.white,
+                            )),
+                        SizedBox(
+                          width: 20.w,
+                        ),
+                        Text(
+                          'My Wallet',
+                          style: TextStyle(
+                              color: AppColor.white,
+                              fontSize: 25.sp,
+                              fontFamily: 'Mont',
+                              fontWeight: FontWeight.w700),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10.h),
+                      child: DefaultCircleAvatar(
+                          imageName: AssetsManager.person, radius: 40.r),
                     ),
                     Text(
-                      '\$50',
+                      'Hossam Saeed',
                       style: TextStyle(
-                          fontWeight: FontWeight.w400,
+                          color: AppColor.white,
+                          fontSize: 25.sp,
                           fontFamily: 'Mont',
-                          fontSize: 35.sp),
+                          fontWeight: FontWeight.w600),
                     ),
-                    DefaultButton(
-                        label: 'Add coin',
-                        radius: 22,
-                        onPressed: () {},
-                        height: 40.h,
-                        width: 110.w)
+                    SizedBox(
+                      height: 20.h,
+                    )
                   ],
                 ),
               ),
-            ),
-          ],
+
+              /// your wallet balance , 50$ , add coin Button
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Container(
+                  width: 375.w,
+                  height: 175.h,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(22),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(.5),
+                        offset: Offset(0.0, 1.0), //(x,y)
+                        blurRadius: 3.0,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Your Wallet balance',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Mont',
+                            fontSize: 20.sp),
+                      ),
+                      Text(
+                        '\$50',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Mont',
+                            fontSize: 35.sp),
+                      ),
+                      DefaultButton(
+                          label: 'Add coin',
+                          radius: 22,
+                          onPressed: () {},
+                          height: 40.h,
+                          width: 110.w)
+                    ],
+                  ),
+                ),
+              ),
+
+              /// credit image
+              Container(
+                height: 265.h,
+                width: 400.w,
+                child: PageView.builder(
+                    controller: controller,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return Image.asset(AssetsManager.credit);
+                    }),
+              ),
+
+              /// indicator
+              DefaultIndicator(controller: controller, length: 3),
+
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 30.h),
+                child: DefaultButton(
+                    label: 'Add Card',
+                    onPressed: () {},
+                    height: 45.h,
+                    width: 310.w),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/presentation/test.dart
+++ lib/presentation/test.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:oz_go_driver/presentation/screens/user/infite_firend_screen.dart';
-import 'package:oz_go_driver/presentation/view/contact_us_view.dart';
-import 'package:oz_go_driver/presentation/view/language_view.dart';
-
-import 'package:oz_go_driver/presentation/view/ride_history.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import 'package:oz_go_driver/presentation/view/app_drawer.dart';
-import 'package:oz_go_driver/presentation/view/satrt_trip_view.dart';
-import 'package:oz_go_driver/presentation/view/terms_view.dart';
-import 'package:oz_go_driver/presentation/widget/custom_app_bar.dart';
-
+import 'package:oz_go_driver/presentation/screens/user/trip_request_screen.dart';
 
 class Test extends StatefulWidget {
   const Test({Key? key}) : super(key: key);
@@ -21,11 +10,9 @@ class Test extends StatefulWidget {
 
 class _TestState extends State<Test> {
   var scaffoldKey = GlobalKey<ScaffoldState>();
-  var controller = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return InviteFriendScreen();
-
+    return TripRequestScreen();
   }
 }
