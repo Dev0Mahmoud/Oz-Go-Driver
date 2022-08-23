cb414a1f0f2fba40529c75eda8628a0bfc4cbea8
MohanadRaghebDev <mohanad.ragheb.95@gmail.com>
2022-08-23 16:09:15 +0200

    start_screen

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../presentation/widget/regular_text.dart';
 
-Widget settings (BuildContext context , {String? name , String? number}) => Column(
-  children: [
-    RegularText(text: name!,),
-    RegularText(text: number!,),
-  ],
-);
+Widget settings(BuildContext context, {String? name, String? number}) => Column(
+      children: [
+        RegularText(
+          text: name!,
+          fontFamily: 'Mont',
+          fontWeight: FontWeight.w300,
+          fontSize: 16.sp,
+        ),
+        RegularText(
+          text: number!,
+          fontFamily: 'Mont',
+          fontWeight: FontWeight.w400,
+          fontSize: 16.sp,
+        ),
+      ],
+    );
