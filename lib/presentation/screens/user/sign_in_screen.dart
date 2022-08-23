5985e10fa48de6df018505ade991250d7bf19ba1
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-08-23 20:08:00 +0200

    sign in with not illed container

--- lib/presentation/screens/user/sign_in_screen.dart
+++ lib/presentation/screens/user/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:oz_go_driver/presentation/view/sign_in_option_button_view.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -11,32 +12,33 @@ class SignInScreen extends StatelessWidget {
       child: Scaffold(
         body: Stack(
           children: [
-            Positioned(
-              top: 0,
-              child: Expanded(
-                child: Container(
-                  width: double.infinity,
-                  height: 220.h,
-                  child: SvgPicture.asset(
-                    'assets/background/background.svg',
-                    fit: BoxFit.fitWidth,
-                  ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: double.infinity,
+                height: 220.h,
+                child: SvgPicture.asset(
+                  'assets/background/background.svg',
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment(0, -0.5),
+              child: Container(
+                width: 350.w,
+                height: 380.h,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.r),
                 ),
               ),
             ),
             Positioned(
-              top: 125.h,
+              bottom: 50.h,
               left: 15.w,
-              right: 15.w,
-              bottom: 100.h,
-              child: Expanded(
-                child: Container(
-                  width: 350.w,
-                  height: 500.h,
-                  color: Color.fromARGB(255, 8, 104, 11),
-                ),
-              ),
-            ),
+              child: SignInOptionButtonView(),
+            )
           ],
         ),
       ),
