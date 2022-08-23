8d08078a0cda8ebd9e48f3f630c4e2a1264203b5
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2022-08-23 17:26:50 +0200

    merge

--- lib/presentation/test.dart
+++ lib/presentation/test.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 
 import 'package:oz_go_driver/presentation/view/ride_history.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:oz_go_driver/presentation/view/app_drawer.dart';
 import 'package:oz_go_driver/presentation/view/otp.dart';
 import 'package:oz_go_driver/presentation/widget/custom_app_bar.dart';
-=======
 import 'package:oz_go_driver/presentation/screens/user/trip_start_screen.dart';
->>>>>>> 4fb91234d907b88ad5fac7e399efa6af939580e9
 
 class Test extends StatefulWidget {
   const Test({Key? key}) : super(key: key);
@@ -22,23 +19,13 @@ class _TestState extends State<Test> {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return Scaffold(
-<<<<<<< HEAD
       body: CustomAppBar(
         isShortArrow: true,
         title: 'Test',
         textAlign: TextAlign.center,
         height: 100.h,
       ),
-=======
-      key: scaffoldKey,
-      drawer: AppDrawer(),
-      body: Center(child: Otp()),
->>>>>>> 5e5c47b84a99028d8a3f619e6585af7dfbfae82b
     );
-=======
-    return TripStartScreen();
->>>>>>> 4fb91234d907b88ad5fac7e399efa6af939580e9
   }
 }
